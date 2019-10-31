b44a789e17755d5a42b1ebc5b7731f2c29f30918
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2019-10-31 13:10:15 +0100

    Fachbegriffe und zweiter Entwurf Kartenautomat

---
+++ Fachbegriffe.md
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+private Scanner tastaturEingabe;
+
+
+# Fachbegriffe Informatik
+
+* Klasse (class)
+* Instanz/Objekt
+* Variable
+    * Datentyp
+        * Integer
+        * String
+    * Wertzuweisung
+        * Ausdruck
+            * Operator
+            * Wert
+    * Arten
+        * private Instanzvariable
+        * lokale Variable
+* Funktion/Methode
+    * Parameter
+    * Rückgabe
+        * Rückgabetyp
+        * Rückgabewert
+    * Block
+    * Konstruktor
+* Kontrollstrukturen
+    * Bedingung
+    * Schleife
+
+Java-Schlüsselwörter
+* public
+* void
+* Scanner
--- GK12-Einstieg/KartenAutomat.java
+++ GK12-Einstieg/KartenAutomat.java
@@ -1,23 +1,80 @@
+import java.util.Scanner;
+
 public class KartenAutomat {
-   private int kartenpreis;
-   private int einbezahlterBetrag;
-   private int anzahlKarten;
-   private int gesamtpreis;
-   private int wechselgeld;
+    private Scanner tastaturEingabe;
+    private int kartenpreis;
+    private int einbezahlterBetrag;
+    private int anzahlKarten;
+    private int gesamtpreis;
+    private int wechselgeld;
+
+    // Konstruktor
+    public KartenAutomat() {
+        tastaturEingabe = new Scanner(System.in);
+        kartenpreis = 37;
+        hauptprogramm();
+    }
+
+    public void zeigeMenu() {
+        einbezahlterBetrag = 0;
+
+        System.out.println("Willkommen im Phantaseeland");
+        System.out.println("Eine Karte kostet " + kartenpreis + " €");
+    }
+
+    public void berechneGesamtpreis() {
+        gesamtpreis = kartenpreis * anzahlKarten;
+        System.out.println("Sie müssen " + gesamtpreis + " € bezahlen");
+    }
+
+    public void nimmGeldAn() {
+        // Der Benutzer wirft nacheinander Euro-Münzen oder Scheine ein
+        // bis genügend Geld eingeworfen wurde
+        // TODO: nimm nur gültige Werte an: 1, 2, 5, 10, 20, 50, 100
+        while (einbezahlterBetrag < gesamtpreis) {
+            System.out.println("Es fehlen noch " + 
+                (gesamtpreis - einbezahlterBetrag) + " €.");
+            System.out.print("Welche Münze oder welchen Schein " +
+                "werfen Sie ein? ");
+            int wert = tastaturEingabe.nextInt();
+            einbezahlterBetrag = einbezahlterBetrag + wert;
+        }
+    }
+
+    public void ermittleAnzahl() {
+        System.out.print("Wieviele Karten möchten Sie? ");
+        anzahlKarten = tastaturEingabe.nextInt();
+    }
+
+    public void gibWechselgeld() {
+        int wechselgeld = einbezahlterBetrag - gesamtpreis;
+        
+        if (wechselgeld > 0) {
+            System.out.println("Sie bekommen noch " + wechselgeld 
+                + " € zurück.");
+            // TODO: Gib Wechselgeld in echten Scheinen/Münzen
+            // gestückelt zurück
+        }
+    }
+
+    public void druckeKarte() {
+        System.out.println("#########################################");
+        System.out.println("# Eine Karte fürs Phantaseeland         #");
+        System.out.println("#########################################"); 
+    }
+
+    public void druckeKarten() {
+        // Solange wie noch nicht alle Karten gedruckt sind
+        // Drucke eine Karte
+        // Verringere die Anzahl noch zu druckender Karten um eins
+    }
     
-   public KartenAutomat() {
-       zeigeMenu();
-   }
-   
-   public void druckeKarte() {}
-   
-   public void zeigeMenu() {}
-   
-   public void berechneGesamtpreis() {}
-   
-   public void nimmGeldAn() {}
-   
-   public void ermittleAnzahl() {}
-   
-   public void gibWechselgeld() {}
+    public void hauptprogramm() {
+        zeigeMenu();
+        ermittleAnzahl();
+        berechneGesamtpreis();
+        nimmGeldAn();
+        gibWechselgeld();
+        druckeKarten();
+    }
 }
